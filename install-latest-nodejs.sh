25146358fd07cfc87d42b2092ab0ea09f83c0835
David Porathur <93052495+41vi4p@users.noreply.github.com>
2024-11-14 21:43:56 +0530

    Create install-latest-nodejs.sh

---
+++ install-latest-nodejs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Installing using nvm 
+# Refer this documentation for further customisation: https://www.linode.com/docs/guides/how-to-install-use-node-version-manager-nvm/
+
+sudo apt install curl
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+source ~/.bashrc
+
+# Dislpay the installed version of nvm package manager
+nvm --version
+
+# Install the latest version of node to customise this command refer the docs
+nvm install node 
+
+node --version
+
+npm --version
