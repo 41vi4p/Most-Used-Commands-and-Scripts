e01c35da285d55d15e671249d7d212004d44ebdf
David Porathur <93052495+41vi4p@users.noreply.github.com>
2024-12-21 11:59:01 +0530

    Create vmware-installer-ubuntu.sh

---
+++ vmware-installer-ubuntu.sh
@@ -0,0 +1,12 @@
+##Installing vmware on ubuntu 24.04
+
+#Installs all required dependencies
+sudo apt install curl gcc-12 git libgcc-12-dev build-essential linux-headers-$(uname -r) -y
+
+#Assuming vmware was downloaded into the Downloads folder
+cd
+cd Downloads
+
+#make the file executable
+chmod +x VMware*.bundle
+
