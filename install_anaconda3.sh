69fa0cbc7a52ac8a95305f60917ebb5d181d8e3a
David Porathur <93052495+41vi4p@users.noreply.github.com>
2025-01-27 14:47:19 +0530

    Create install_anaconda3.sh

---
+++ install_anaconda3.sh
@@ -0,0 +1,8 @@
+#Download and Install Anaconda (1GB approx)
+#Note: Avoid running this script as root or 'sudo'
+
+wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+
+chmod +x Anaconda3-2024.10-1-Linux-x86_64.sh
+
+./Anaconda3-2024.10-1-Linux-x86_64.sh
