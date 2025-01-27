12d0763fedc38c1d5c5056e93b245ec8fa543dd2
David Porathur <93052495+41vi4p@users.noreply.github.com>
2025-01-27 14:45:24 +0530

    Create install_miniconda3.sh

---
+++ install_miniconda3.sh
@@ -0,0 +1,7 @@
+#Download and Install Miniconda3 
+# Note: Avoid running this script as root or 'sudo'!!!
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+
+./Miniconda3-latest-Linux-x86_64.sh
