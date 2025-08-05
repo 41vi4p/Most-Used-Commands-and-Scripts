8dfda06fd78008fa636bc01ccd3f9e7292fa7a10
David Porathur <93052495+41vi4p@users.noreply.github.com>
2025-08-05 14:14:00 +0530

    Create install_vscode.sh

---
+++ install_vscode.sh
@@ -0,0 +1,6 @@
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+sudo apt update
+sudo apt install code
