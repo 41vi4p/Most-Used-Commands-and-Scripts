3e65e60b27e5c9cf080eb5739c079120dcfb4658
David Porathur <93052495+41vi4p@users.noreply.github.com>
2026-05-19 20:28:56 +0530

    Add Spotify repository and installation commands

---
+++ install_spotify.sh
@@ -0,0 +1,6 @@
+#add repository
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+#install spotify using apt
+sudo apt-get update && sudo apt-get install spotify-client
